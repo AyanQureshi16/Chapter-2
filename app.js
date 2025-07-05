cc1d08221cfd365080bab15716aa65e3f62df6f6
Ayan Shahid <ayanshahid1612@gmail.com>
2025-07-05 17:23:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+// Chapter 2
+
+var Name =("Ayan Shahid")
+var age =("17")
+var course =("Web And App Development")
+var email =("My Email Address is ayanshahid1612@gmail.com")
+var pizza =("Pizza\nPizz\nPiz\nPi\nP")
+
+
+
+alert(Name)
+alert(age)
+alert(course)
+alert(email)
+alert(pizza)
+
+document.write("yah.. i can write html through javascript")
+
